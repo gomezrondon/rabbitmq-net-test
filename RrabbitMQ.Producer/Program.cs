aebb97e07fa323878775aa51b3d8cd1eab6fdf53
gomezrondon <gomezrondon@gmail.com>
2020-12-16 16:12:30 -0600

    Refactoring producer

--- RrabbitMQ.Producer/Program.cs
+++ RrabbitMQ.Producer/Program.cs
@@ -16,16 +16,8 @@ namespace RrabbitMQ.Producer
 
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            //declare a queue
-            channel.QueueDeclare("demo-queue",
-                durable: true,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null);
-            var message = new {Name = "Producer", Message = "Hello!"};
-            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
             
-            channel.BasicPublish("","demo-queue",null, body);
+            QueueProducer.Publisher(channel);
         }
     }
 }
---
+++ RrabbitMQ.Producer/QueueProducer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RrabbitMQ.Producer
+{
+    public class QueueProducer
+    {
+        public static void Publisher(IModel channel)
+        {
+            //declare a queue
+            channel.QueueDeclare("demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var message = new {Name = "Producer", Message = "Hello!"};
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            
+            channel.BasicPublish("","demo-queue",null, body);
+        }
+    }
+}
